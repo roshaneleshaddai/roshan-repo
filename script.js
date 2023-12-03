d537119d41d06dfe4cd32e26f8e702a0c078f674
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2023-12-03 20:54:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+function updateTimer() {
+    const futureDate = new Date("2023-12-07T09:00:00");
+    const currentDate = new Date();
+    const timeDifference = futureDate - currentDate;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    daysElement.textContent = formatTime(days);
+    hoursElement.textContent = formatTime(hours);
+    minutesElement.textContent = formatTime(minutes);
+    secondsElement.textContent = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? 0:{time}, time;
+}
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
